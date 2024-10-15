b99244db09e35d655750dea9ea480354100613b2
Pem Chettar Sherpa <pemshera@gmail.com>
2024-10-15 16:28:36 +0545

    changed to sqlite

--- travelticketing/settings.py
+++ travelticketing/settings.py
@@ -75,20 +75,20 @@ WSGI_APPLICATION = 'travelticketing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-import os
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# import os
+# import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL')
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
