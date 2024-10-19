0d887718e194a125f1ef2b295df22c4280365abb
Pem Chettar Sherpa <pemshera@gmail.com>
2024-10-19 19:21:37 +0545

    cors fixing

--- travelticketing/settings.py
+++ travelticketing/settings.py
@@ -34,7 +34,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'https://busgobus.vercel.app',
-    'http://localhost:5173/',
+    'http://localhost:5173',
     # Add the domains you want to allow
 ]
 
